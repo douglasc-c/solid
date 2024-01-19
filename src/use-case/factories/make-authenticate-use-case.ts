f847881194edf924609506ca63e65344c01b1ce8
Douglas Camargo <douglas.camargo.ltda@gmail.com>
2024-01-19 20:04:23 -0300

    update project

--- src/use-case/factories/make-authenticate-use-case.ts
+++ src/use-case/factories/make-authenticate-use-case.ts
@@ -1,4 +1,4 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { AuthenticateUseCase } from '../authenticate'
 
 export function makeAuthenticateUseCase() {
--- src/use-case/factories/make-register-use-case.ts
+++ src/use-case/factories/make-register-use-case.ts
@@ -1,4 +1,4 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { RegisterUseCase } from '../register'
 
 export function makeRegisterUseCase() {
